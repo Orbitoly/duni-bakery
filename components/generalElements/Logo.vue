2bbca73f8c22cfa98282a738269184e56215931e
Orbitoly <orvirt@gmail.com>
2023-05-13 16:27:31 +0300

    Update Logo.vue

--- components/generalElements/Logo.vue
+++ components/generalElements/Logo.vue
@@ -1,3 +1,9 @@
+<script setup>
+const runtimeConfig = useRuntimeConfig();
+const logoUrl = runtimeConfig?.public?.LOGO || null;
+</script>
+
+<template>
   <NuxtLink to="/">
     <img v-if="logoUrl" :src="logoUrl" alt="Logo" class="object-contain h-10" />
     <div v-else class="flex font-bold text-lg gap-2 items-center">
